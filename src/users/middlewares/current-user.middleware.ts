548770aaccbe2771523baac5b4fad7224d628fdc
abandu <banduraweb@ukr.net>
2022-02-01 19:40:37 +0200

    fix type Request

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -2,21 +2,26 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
 import { User } from '../user.entity';
+import { Express } from 'express/ts4.0';
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Express {
-    interface Request {
-      currentUser?: User;
-    }
-  }
+// declare global {
+//   // eslint-disable-next-line @typescript-eslint/no-namespace
+//   namespace Express {
+//     interface Request {
+//       currentUser?: User;
+//     }
+//   }
+// }
+
+interface iRequest extends Request {
+  currentUser?: User;
 }
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: iRequest, res: Response, next: NextFunction) {
     // @ts-ignore
     const { userId } = req.session || {};
 
